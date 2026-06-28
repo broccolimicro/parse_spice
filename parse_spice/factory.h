29cdef91c8cdb1cfa2e122828e2445d3baacb1cc
Ned Bingham <edward.bingham@broccolimicro.io>
2026-06-28 12:33:13 -0400

    using new parse factory structure

--- parse_spice/factory.h
+++ parse_spice/factory.h
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <parse/parse.h>
-#include <parse/syntax.h>
+#include <parse/factory.h>
 
 namespace parse_spice {
 
-parse::syntax *produce(tokenizer &tokens, void *data=nullptr);
-void expect(tokenizer &tokens);
-void register_syntax(tokenizer &tokens);
+extern const parse::factory factory;
 
 }
 
